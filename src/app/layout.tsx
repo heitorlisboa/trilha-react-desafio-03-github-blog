2222f768137df4fcdc800ce490e8fa52963da84f
heitorlisboa <heitor01101000@gmail.com>
2023-03-17 02:44:47 -0300

    fix: center logo in the page header
    * Centered the logo in the page header. It became off-center when I removed its wrapping `h1` tag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => (
     <body className="isolate bg-slate-950 leading-relaxed text-slate-300 [-moz-osx-font-smoothing:grayscale] [-webkit-font-smoothing:antialiased]">
       <header className="relative -z-10 -mb-24 flex h-72 items-center justify-center overflow-hidden bg-slate-900">
         <HeaderLinesLeft className="absolute left-0" aria-hidden />
-        <Logo
-          className="absolute top-16 -translate-x-1/2"
-          aria-label="GitHub Blog"
-        />
+        <Logo className="absolute top-16" aria-label="GitHub Blog" />
         <HeaderLinesRight
           className="absolute right-0 hidden md:block"
           aria-hidden
